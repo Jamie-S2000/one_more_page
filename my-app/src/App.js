ab3795f8b42567c194060aee013a1d7915bbf828
Jamie Sewell <jamiegsewell@gmail.com>
2025-06-13 01:43:19 +0100

    feat: adds routing to the navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,21 @@
-import styles from './App.module.css'
-import NavBar from './components/NavBar';
-
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/login" element={<h1>Log in</h1>} />
+          <Route path="/register" element={<h1>Register</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+          <Route path="*" element={<p>Page not found!</p>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,19 +1,48 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import styles from '../styles/NavBar.module.css';
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top" variant="dark">
       <Container>
-        <Navbar.Brand href="#home"><i class="fa-solid fa-book-open"></i> One More Page</Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand href="#home">
+            <i class="fa-solid fa-book-open"></i> One More Page
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Log in</Nav.Link>
-            <Nav.Link href="#link">Register</Nav.Link>
-            <Nav.Link href="#link">Contact</Nav.Link>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/login"
+            >
+              Log in
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/register"
+            >
+              Register
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/contact"
+            >
+              Contact
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
