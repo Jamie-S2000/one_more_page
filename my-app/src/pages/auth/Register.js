54a6bba48d8ee31b1d251f6a38c4f0a5cc988763
Jamie Sewell <jamiegsewell@gmail.com>
2025-06-16 11:56:25 +0100

    feat: sets up registration

--- my-app/src/pages/auth/Register.js
+++ my-app/src/pages/auth/Register.js
@@ -1,18 +1,57 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import { Form, Button, Container } from "react-bootstrap";
+import { Form, Button, Container, Alert } from "react-bootstrap";
+import axios from "axios";
 
 const Register = () => {
+  const [registerData, setRegisterData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = registerData;
+
+  const [errors, setErrors] = useState({});
+
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setRegisterData({
+      ...registerData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration", registerData);
+      navigate("/login");
+    } catch (err) {
+      console.log("Registration error:", err.response?.data);
+      setErrors(err.response?.data);
+    }
+  };
+
   return (
     <div>
       <Container>
         <h1>Register</h1>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group controlId="username">
             <Form.Label className="d-none">username</Form.Label>
-            <Form.Control type="text" placeholder="username" name="username" />
+            <Form.Control
+              type="text"
+              placeholder="username"
+              name="username"
+              value={username}
+              onChange={handleChange}
+            />
           </Form.Group>
+          {errors.username?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>{message}</Alert>
+          ))}
 
           <Form.Group controlId="password1">
             <Form.Label className="d-none">Password</Form.Label>
@@ -20,6 +59,8 @@ const Register = () => {
               type="password"
               placeholder="Password"
               name="password1"
+              value={password1}
+              onChange={handleChange}
             />
           </Form.Group>
 
@@ -29,6 +70,8 @@ const Register = () => {
               type="password"
               placeholder="Confirm Password"
               name="password2"
+              value={password2}
+              onChange={handleChange}
             />
           </Form.Group>
           <Button variant="primary" type="submit">
