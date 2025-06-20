05678339e2ce2b2b4174ac4a205197cd507d6749
Jamie Sewell <jamiegsewell@gmail.com>
2025-06-20 02:06:29 +0100

    feat: adds new navbar links and allows users to sign out

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -2,12 +2,53 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const handleLogOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const newPostIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/newpost"
+    >
+      New post
+    </NavLink>
+  );
+  const loggedInIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/"
+        onClick={handleLogOut}
+      >
+        Log Out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        My Profile
+      </NavLink>
+    </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink
@@ -45,6 +86,7 @@ const NavBar = () => {
             >
               Home
             </NavLink>
+            {currentUser && newPostIcon}
             {currentUser ? loggedInIcons : loggedOutIcons}
             <NavLink
               className={styles.NavLink}
