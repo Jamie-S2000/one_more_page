f8bd6182fafc97f4dc9f77e01576de49ffe859a3
Jamie Sewell <jamiegsewell@gmail.com>
2025-06-25 10:52:26 +0100

    feat: adds profile link to comments

--- my-app/src/pages/comments/Comment.js
+++ my-app/src/pages/comments/Comment.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import formStyles from "../../styles/Forms.module.css";
 
 function Comment({ owner, content }) {
   return (
     <div className={formStyles.Div}>
-      {owner}: {content}
+      <Link className={formStyles.Link} to={`/profiles/${owner}`}>{owner}</Link>: {content}
     </div>
   );
 }
